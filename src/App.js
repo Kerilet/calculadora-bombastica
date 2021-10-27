f3d8a89ad688fa0ee36bc63a899b37e84d5740c2
Kerilet <jivore78@gmail.com>
2021-10-27 10:34:34 -0300

    added the torture dance easter egg;

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/media-has-caption */
 /* eslint-disable max-len */
 import React, { useState } from 'react';
 import Calculator from './components/Calculator';
@@ -8,6 +9,50 @@ function App() {
   const [total, setTotal] = useState(0);
   const [tip, setTip] = useState(0);
   const [reseted, setReseted] = useState(new Date());
+
+  let codes1 = [];
+
+  const konamiKode = [
+    'ArrowUp',
+    'ArrowUp',
+    'ArrowDown',
+    'ArrowDown',
+    'ArrowLeft',
+    'ArrowRight',
+    'ArrowLeft',
+    'ArrowRight',
+    'b',
+    'a',
+    'Enter',
+  ].join('-');
+
+  const playAudio = () => {
+    const audio = document.getElementById('audio1');
+    audio.play();
+  };
+
+  const writeKonamiCode = () => {
+    const tortureDanceDIV = document.getElementById('easterEgg');
+    tortureDanceDIV.innerHTML += '<img src="https://thumbs.gfycat.com/AllConstantChital-size_restricted.gif" alt="Konami Kode duuude!" loading="here\'s an easter egg!">';
+    playAudio();
+  };
+
+  const konamiCheck = (callback) => {
+    document.addEventListener('keyup', (event) => {
+      codes1.push(event.key);
+      const joined = codes1.join('-');
+      if (konamiKode.startsWith(joined)) {
+        if (konamiKode === joined) {
+          callback();
+        }
+      } else {
+        codes1 = [];
+      }
+      console.log(codes1);
+    });
+  };
+
+  konamiCheck(writeKonamiCode);
 
   return (
     <div className={style.App}>
@@ -36,7 +81,8 @@ function App() {
         Challenge by Frontend Mentor.Coded by Kerilet.
 
       </div>
-
+      <div id="easterEgg" className={style.easterEgg} />
+      <audio id="audio1" src="./sfx/canzoniPreferite.mp3" />
     </div>
   );
 }
